44460051cb1e4519708cc1219d0241df54e78a07
Dyllon Symington <dyllon.leigh@gmail.com>
2025-06-28 17:05:08 +0200

    done

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ with open("./Input/Letters/starting_letter.txt") as file:
 with open("./Input/Names/invited_names.txt") as file:
     for name in file:
         custom_letter = letter_data.replace("[name]", name.strip())
-        letter = f"./Output/ReadyToSend/{name.strip()}'s letter.txt"
-        with open(letter, mode="w") as output_file:
+        with open(f"./Output/ReadyToSend/letter_for_{name.strip()}.txt", mode="w") as output_file:
             output_file.write(custom_letter)
